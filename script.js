323b5e89190eef2c23d19d18cafb9c6cb1b3d6ed
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-08-30 01:25:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function toCase(text) {
   // write your code here
-	if(!text || text.length>0) {
+	if(!text ) {
 		return "-";
 	}
 	text = text.trim();
 	if(text.length === 0) {
 		return "-";
 	}
-	return text.toLowerCase() "-" text.toUpperCase();
+	return text.toLowerCase() + "-" +text.toUpperCase();
 }
 
 // DO not change the code below
